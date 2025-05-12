aecb61fd865d9a68cdce2ffe277db8e1a11c2568
AleevDigital <aleevpj@gmail.com>
2025-05-12 19:44:43 -0300

    Conferencia Ranking STN v1.2
    Alterações Visuais

--- interfaces/pages/Conferência Ranking STN.py
+++ interfaces/pages/Conferência Ranking STN.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from datetime import datetime
 
+st.set_page_config(layout='wide',initial_sidebar_state = 'collapsed',page_icon ='..\..\Logo Aleev.jpg')
 year = datetime.now().strftime("%Y")
 last_year = int(year)-1
 
@@ -19,13 +20,18 @@ with st.container():
         st.subheader('D4')
         st.write('Matriz usada para preenchimento da sheet D4 na planilha de conferência')
         matriz_d4 = st.file_uploader("Escolha um arquivo para a extração do D4", type="csv")
+    
+    st.markdown("""
+<div style="background-color:#f0f2f6; padding:4px; border-radius:10px">
+</div>
+""", unsafe_allow_html=True)
 
 with st.container():
-    col1,col2 = st.columns(2)
+    col1,col2, col3 = st.columns(3)
 
     with col1:
         st.subheader('DCA')
-        st.write('Matriz usada para preenchimento da sheet D2 na planilha de conferência')
+        st.write('Matriz usada para preenchimento referente ao dca na planilha de conferência')
         matriz_dca = st.file_uploader("Escolha um arquivo para a extração do DCA", type="csv")
         
     with col2:
@@ -33,18 +39,22 @@ with st.container():
         st.write('Matriz usada para preenchimento referente ao RREO na planilha de conferência')
         matriz_dca = st.file_uploader("Escolha um arquivo para a extração do RREO", type="csv")
 
-with st.container():
-    col1,col2 = st.columns(2)
-
-    with col1:
+    with col3:
         st.subheader('RGF')
         st.write('Matriz usada para preenchimento referente ao RGF na planilha de conferência')
         matriz_dca = st.file_uploader("Escolha um arquivo para a extração do RGF", type="csv")
-        
-    with col2:
-        st.subheader('Planilha de Conferência')
-        st.write('Matriz usada para preenchimento da sheet D2 na planilha de conferência')
-        matriz_dca = st.file_uploader("Escolha o arquivo que será preenchido", type="csv")
+
+    st.markdown("""
+<div style="background-color:#f0f2f6; padding:4px; border-radius:10px">
+</div>
+""", unsafe_allow_html=True)
+
+with st.container():
+    st.subheader('Planilha de Conferência')
+    st.write('Matriz usada para preenchimento da sheet D2 na planilha de conferência')
+    matriz_dca = st.file_uploader("Escolha o arquivo que será preenchido", type="csv")
+
+
 
 with st.container():
     col1, col2, col3 = st.columns([1.5, 1, 1])  # col2 é maior (no centro)
