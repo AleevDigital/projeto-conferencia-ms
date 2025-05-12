c4af0976b5ead213951a772bae2cdceab2bb1406
AleevDigital <aleevpj@gmail.com>
2025-05-12 17:43:00 -0300

    Conferência Ranking STN v1.1
    Estruturação da interface visual da aba Conferência Ranking STN: foram colocados todos os botões, colunas e containers que vão compor a página do site, restando apenas colocar as instruções e pequenos ajustes para melhorar a visualização

--- interfaces/pages/Conferência Ranking STN.py
+++ interfaces/pages/Conferência Ranking STN.py
@@ -2,7 +2,53 @@ import streamlit as st
 from datetime import datetime
 
 year = datetime.now().strftime("%Y")
-print(year)
+last_year = int(year)-1
 
-st.title(f'Conferência Ranking STN {year}')
-st.write("Seja bem vindo!")
+st.title(f'Conferência Ranking STN {last_year}')
+st.write("Instruções")
+
+with st.container():
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader('D2')
+        st.write('Matriz usada para preenchimento da sheet D2 na planilha de conferência')
+        matriz_d2 = st.file_uploader("Escolha um arquivo para a extração do D2", type="csv")
+     
+    with col2:
+        st.subheader('D4')
+        st.write('Matriz usada para preenchimento da sheet D4 na planilha de conferência')
+        matriz_d4 = st.file_uploader("Escolha um arquivo para a extração do D4", type="csv")
+
+with st.container():
+    col1,col2 = st.columns(2)
+
+    with col1:
+        st.subheader('DCA')
+        st.write('Matriz usada para preenchimento da sheet D2 na planilha de conferência')
+        matriz_dca = st.file_uploader("Escolha um arquivo para a extração do DCA", type="csv")
+        
+    with col2:
+        st.subheader('RREO')
+        st.write('Matriz usada para preenchimento referente ao RREO na planilha de conferência')
+        matriz_dca = st.file_uploader("Escolha um arquivo para a extração do RREO", type="csv")
+
+with st.container():
+    col1,col2 = st.columns(2)
+
+    with col1:
+        st.subheader('RGF')
+        st.write('Matriz usada para preenchimento referente ao RGF na planilha de conferência')
+        matriz_dca = st.file_uploader("Escolha um arquivo para a extração do RGF", type="csv")
+        
+    with col2:
+        st.subheader('Planilha de Conferência')
+        st.write('Matriz usada para preenchimento da sheet D2 na planilha de conferência')
+        matriz_dca = st.file_uploader("Escolha o arquivo que será preenchido", type="csv")
+
+with st.container():
+    col1, col2, col3 = st.columns([1.5, 1, 1])  # col2 é maior (no centro)
+
+    with col2:
+        if st.button('INICIAR'):
+            print('Processo Iniciado')
